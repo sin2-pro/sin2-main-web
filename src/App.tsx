eef1464d7475844a6d06b8d3dc6921c8e5c8bc4c
sin2 <sin.2.shan@gmail.com>
2025-06-20 22:48:43 -0400

    update bg and copy

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,9 @@ function App() {
       <p>Developer. Artist. Vintage film romantic. I build apps that blend design, utility, and narrative magic.</p>
       
       <nav>
-        <a href="https://github.com/yourhandle">GitHub</a>
-        <a href="https://twitter.com/yourhandle">Twitter</a>
-        <a href="mailto:you@example.com">Email</a>
+        <a href="https://github.com/sin2">GitHub</a>
+        <a href="mailto:contact@sin2.ca">Email</a>
       </nav>
-
-      <section className="apps">
-        <h2>Apps</h2>
-        <a href="#">SkyOS – Satirical mobile OS sim</a>
-        <a href="#">YumYum – AI calorie tracker</a>
-      </section>
     </main>
   );
 }
