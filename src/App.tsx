eb2b02fe05eb0310b77b72d8724f04d7188490de
sin2 <sin.2.shan@gmail.com>
2025-06-21 23:56:04 -0400

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -7,26 +7,31 @@ function App() {
         <h1>Sinthu S.</h1>
         <p className="tagline">Software Developer and Artist in Ontario, Canada</p>
       </header>
-      
+
       <section className="about">
         <p>
-          As a software developer and artist, I thrive on creation, experimentation, and continuous learning.
+          As a software developer and artist, I thrive on creation, experimentation, and continuous
+          learning.
         </p>
         <p>
-          My passion lies at the intersection of computer science and fine arts, where I explore innovative ways to bridge these disciplines.
+          My passion lies at the intersection of computer science and fine arts, where I explore
+          innovative ways to bridge these disciplines.
         </p>
         <p>
-          Through technology and the creative process, I aim to challenge perspectives, spark curiosity, and craft experiences that inspire thought and discovery.
+          Through technology and the creative process, I aim to challenge perspectives, spark
+          curiosity, and craft experiences that inspire thought and discovery.
         </p>
       </section>
 
-      <h1 className="text-3xl underline text-red-500 bg-blue-500">
-            Hello world!  
-      </h1>
-      
+      <h1 className="text-3xl underline text-red-500 bg-blue-500">Hello world!</h1>
+
       <nav className="social-links">
-        <a href="https://github.com/sin2" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="http://www.linkedin.com/in/ssivapat" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://github.com/sin2" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        <a href="http://www.linkedin.com/in/ssivapat" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
         <a href="mailto:contact@sin2.ca">Email</a>
       </nav>
     </main>
