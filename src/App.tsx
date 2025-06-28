e4415e2c738f710ef9bb7c69d0a758c2d5bfdd3e
sin2 <sin.2.shan@gmail.com>
2025-06-28 10:59:11 -0400

    updated fonts colors copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useWindowSize } from 'hamo';
 import './App.css';
-import { IoLogoGithub, IoMail, IoLogoLinkedin } from 'react-icons/io5';
+import { IoLogoGithub, IoMail, IoLogoLinkedin, IoArrowDown } from 'react-icons/io5';
 import { useState, useEffect } from 'react';
 
 import CustomCursor from './components/CustomCursor';
@@ -33,34 +33,48 @@ function App() {
 
       <div className="noise bg-gradient-to-b from-neutral-900 to-black" />
       <CustomCursor />
-      <main className="relative z-1">
-        <header className="p-4 prose prose-invert flex flex-col justify-center" style={{ height }}>
-          <h1>Sinthu S.</h1>
-          <h2>Software Developer and Artist</h2>
-          <h3>Ontario, Canada</h3>
+      <main className="flex flex-col relative z-1">
+        <header className="p-4 flex flex-col justify-center" style={{ height }}>
+          <div className="prose prose-invert">
+            <p>
+              <h1>Sinthu S.</h1>
+            </p>
+            <p>
+              <b className="text-accent text-lg">
+                CTO @{' '}
+                <a
+                  className="text-accent"
+                  href="https://www.gamerlinkapp.com/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  GamerLink Inc.
+                </a>
+              </b>
+              <br />
+              <b>Software Developer and Artist</b>
+              <br />
+              Ontario, Canada
+            </p>
+          </div>
 
-          <div className="flex flex-1 flex-col justify-end">
-            <div
-              className={`text-xl text-green-400 self-start flex flex-col items-center select-none pointer-events-none transition-opacity duration-700 ${arrowVisible ? 'opacity-100' : 'opacity-0'}`}
-            >
-              <span className="animate-bounce">↓</span>
-            </div>
+          <div
+            className={`fixed bottom-4 left-4 text-3xl text-accent select-none pointer-events-none transition-opacity duration-700 ${arrowVisible ? 'opacity-100' : 'opacity-0'}`}
+          >
+            <IoArrowDown className="animate-bounce" />
           </div>
         </header>
 
         <section className="p-4 prose prose-invert flex flex-col justify-center" style={{ height }}>
           <h1>Hello world!</h1>
           <p>
-            As a software developer and artist, I thrive on creation, experimentation, and
-            continuous learning.
+            I'm a software developer and artist who enjoys building, exploring, and learning across
+            disciplines.
           </p>
           <p>
-            My passion lies at the intersection of computer science and fine arts, where I explore
-            innovative ways to bridge these disciplines.
-          </p>
-          <p>
-            Through technology and the creative process, I aim to challenge perspectives, spark
-            curiosity, and craft experiences that inspire thought and discovery.
+            I focus on combining <span className="text-accent">tech and visual design</span> in ways
+            that feel fresh and intentional, creating tools, visuals, and ideas that make people
+            look twice or think a little differently.
           </p>
         </section>
 
@@ -68,7 +82,7 @@ function App() {
           <a
             href="https://github.com/sin2"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener"
             className="flex flex-row gap-1 items-center"
           >
             <IoLogoGithub />
@@ -77,7 +91,7 @@ function App() {
           <a
             href="http://www.linkedin.com/in/ssivapat"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener"
             className="flex flex-row gap-1 items-center"
           >
             <IoLogoLinkedin />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,41 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-        display: ['Space Grotesk', 'Inter', 'system-ui', 'sans-serif'],
+        sans: ['Spline Sans Mono', 'sans-serif'],
+        display: ['Syne', 'sans-serif'],
+      },
+      colors: {
+        accent: 'rgba(255, 30, 79)',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              'font-family': 'Syne',
+            },
+            a: {
+              'font-family': 'Spline Sans Mono',
+            },
+            h2: {
+              'font-family': 'Spline Sans Mono',
+            },
+            h3: {
+              'font-family': 'Spline Sans Mono',
+            },
+            h4: {
+              'font-family': 'Spline Sans Mono',
+            },
+            h5: {
+              'font-family': 'Spline Sans Mono',
+            },
+            h6: {
+              'font-family': 'Spline Sans Mono',
+            },
+            p: {
+              'font-family': 'Spline Sans Mono',
+            },
+          },
+        },
       },
     },
   },
